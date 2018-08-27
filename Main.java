39a1171418da6c8230a0dfdc0be7600e122848e5
robertmchugh <42556355+robertmchugh@users.noreply.github.com>
2018-08-27 16:33:59 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,25 @@
+// Robert McHugh
+// CMPSC 221
+
 public class Main 
 {
   
    public static void main(String[] args) 
    {
-      System.out.println("  *  ");
-      System.out.println(" *** ");
-      System.out.println("*****");
+      System.out.println("        .         ");
+      System.out.println("       .H         ");
+      System.out.println("      ,HH         ");
+      System.out.println("      HH:         ");
+      System.out.println(" .    YHH         ");
+      System.out.println(" H    `HHH;     . ");
+      System.out.println(" H;    `HHH    .H ");
+      System.out.println(" HH,  ,HHH    ,HH ");
+      System.out.println(" `HH, HHHH&  ,HY  ");
+      System.out.println("  ,HHH.HHHHH.HH,  ");
+      System.out.println("  HHHHHHHHHHHHHH  ");
+      System.out.println("  HHHHHHHHHHHHHH  ");
+      System.out.println("   HHHHHHHHHHHH   ");
+      System.out.println("     ````````     ");
   }
   
 }
